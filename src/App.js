df0400eeee27ef8dc2b7c0e97f43d1eee36397cf
Shilpa Pandey <shilpakpandey10@gmail.com>
2021-06-08 19:24:14 +0530

    Useformik used to create normal signup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { SignUp } from './components/SignUp';
+
+function App() {
+  return (
+     <div>
+       <SignUp/>
+     </div>
+     
+  
+  );
+}
+
+export default App;
